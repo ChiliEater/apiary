7f940d68e516c183ff33a9e91b41f620a670aee0
Jonas <contact@jonascosta.ch>
2023-10-29 23:29:03 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Apiary
 
 This is an extremely simple dummy API for local testing with [mobile-assignment](https://github.com/ChiliEater/mobile-assignment).
+
+```shell
+yarn
+```
+
+```shell
+yarn run dev
+```
--- src/Main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var dotenv_1 = require("dotenv");
-dotenv_1.default.config();
-var server = (0, express_1.default)();
-console.log(process.env.PORT);
