6fe350adf00dceb38f917a449c3986beec53d3f9
Jonas <contact@jonascosta.ch>
2023-10-30 21:07:23 +0100

    API

--- src/Main.ts
+++ src/Main.ts
@@ -6,11 +6,31 @@ dotenv.config()
 
 const server: Express = express();
 const port: number = parseInt(process.env.PORT as unknown as string);
+const publicRoot: string = process.env.ROOT as unknown as string;
+const cookiePath: RegExp = /\/cookie\/.*?\/?/;
 
 server.get("/", (req: Request, res: Response) => {
     res.send("This is a test wow");
 });
 
+server.use(express.static(publicRoot));
+
 server.listen(port, () => {
     Logger.info(`Server started at http://localhost:${port}`);
+});
+
+server.post(cookiePath, (req: Request, res: Response) => {
+    Logger.info("Received POST");
+    res.json();
+});
+
+server.put(cookiePath, (req: Request, res: Response) => {
+    Logger.info("Received PUT");
+    res.json();
+});
+
+
+server.delete(cookiePath, (req: Request, res: Response) => {
+    Logger.info("Received DELETE");
+    res.json();
 });
